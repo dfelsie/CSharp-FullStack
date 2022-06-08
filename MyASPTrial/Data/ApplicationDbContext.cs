400be0dd96656820a8d2839244bbd6a14b994dcf
dfelsie <dfelsenthal88@gmail.com>
2022-06-08 03:54:59 -0400

    Initial Commit

---
+++ MyASPTrial/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyASPTrial.Models;
+
+namespace MyASPTrial.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MyASPTrial.Models.Joke>? Joke { get; set; }
+    }
+}
